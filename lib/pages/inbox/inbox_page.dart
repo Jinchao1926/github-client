c1497b01e9f986510b7fe80aca890e6996fd7cc8
Jinchao Lin <51687782+Jinchao1926@users.noreply.github.com>
2026-03-23 09:50:47 +0800

    Implement main tab (#2)

---
+++ lib/pages/inbox/inbox_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InboxPage extends StatelessWidget {
+  const InboxPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Inbox')),
+      body: Center(child: Text('Welcome to the Inbox Page!')),
+    );
+  }
+}
